b6cdab354ccc899e86d1f241e7960385ce6387af
corporatetrainer <corporatetrainer21@gmail.com>
2019-07-22 12:20:08 +0530

    Task 1 change 1

---
+++ ConsoleApp1/ConsoleApp1/Task1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Task1
+    {
+        public int add()
+        {
+            return 10 + 2;
+        }
+
+    }
+}
